fec2be8d498573a9fb0da6c890a4b06ab9c9229d
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-16 22:03:01 +0800

    Update myDB.sql

--- dump/myDB.sql
+++ dump/myDB.sql
@@ -1,6 +1,6 @@
 SET time_zone = "+08:00";
 
-CREATE TABLE `producttb` (
+CREATE TABLE `Producttb` (
   `id` int(11) NOT NULL,
   `product_name` varchar(25) NOT NULL,
   `product_price` float DEFAULT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE `producttb` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
-INSERT INTO `producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
+INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
 (5, 'Apple MacBook Pro', 1799, '/upload/product1.png'),
 (6, 'Sony E7 Headphones', 147, '/upload/product1.png'),
 (7, 'Sony Xperia Z4', 459, '/upload/product1.png'),
