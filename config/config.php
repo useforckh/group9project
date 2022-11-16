aed352125e7b796c0d66abc1bbd73e6d9c8d05a8
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-17 00:55:58 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,12 +1,12 @@
 <?php
 	// Database credentials
-	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'login_system');
+	define('DB_SERVER', 'db');
+	define('DB_USERNAME', 'user');
+	define('DB_PASSWORD', 'test');
+	define('DB_NAME', 'myDB');
 
 	// Attempt to connect to MySQL database
-	$mysql_db = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+	$mysql_db = new mysqli('db', 'user', 'test', "myDb");
 
 	if (!$mysql_db) {
 		die("Error: Unable to connect " . $mysql_db->connect_error);
