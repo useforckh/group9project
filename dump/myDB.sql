cec84451cf793e11c0d27c6da444098b81ab3648
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-17 00:59:01 +0800

    Update myDB.sql

--- dump/myDB.sql
+++ dump/myDB.sql
@@ -7,6 +7,12 @@ CREATE TABLE `Producttb` (
   `product_image` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
 (5, 'Apple MacBook Pro', 1799, '/upload/product1.png'),
