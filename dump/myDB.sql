19b7f54f5bad1a0ba2e29b2abb503c4a434e5051
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-19 02:32:13 +0800

    Update myDB.sql

--- dump/myDB.sql
+++ dump/myDB.sql
@@ -7,6 +7,13 @@ CREATE TABLE `Producttb` (
   `product_image` varchar(100) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+
+INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
+(5, 'Apple MacBook Pro', 1799, '/upload/product1.png'),
+(6, 'Sony E7 Headphones', 147, '/upload/product2.png'),
+(7, 'Sony Xperia Z4', 459, '/upload/product3.png'),
+(8, 'Samsung Galaxy A50', 278, '/upload/product4.png');
+
 CREATE TABLE `users` (
   `id` int NOT NULL,
   `username` varchar(50) NOT NULL,
@@ -14,8 +21,5 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`) VALUES
-(5, 'Apple MacBook Pro', 1799, '/upload/product1.png'),
-(6, 'Sony E7 Headphones', 147, '/upload/product2.png'),
-(7, 'Sony Xperia Z4', 459, '/upload/product3.png'),
-(8, 'Samsung Galaxy A50', 278, '/upload/product4.png');
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'abc', '$2y$10$trdS/C/WF1S0aH1gHEmwy.HvlbRIhlpR.psOs676vm0F5xXuOJ.Yi', '2022-11-18 18:30:49');
