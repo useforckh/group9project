532a463cd8a5bced08be822014294092b9db1445
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-19 02:55:25 +0800

    Update main.php

--- www/main.php
+++ www/main.php
@@ -54,6 +54,7 @@ if (isset($_POST['add'])){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Shopping Cart</title>
+    
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
