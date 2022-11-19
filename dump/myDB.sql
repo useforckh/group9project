b75863650a8c4f045b5eb330cc9abf88d43de77b
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-19 22:23:31 +0800

    Update myDB.sql

--- dump/myDB.sql
+++ dump/myDB.sql
@@ -15,12 +15,12 @@ INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`)
 (8, 'Samsung Galaxy A50', 278, '/upload/product4.png');
 
 
-CREATE TABLE 'users' (
-    'id' int(11) NOT ,
-    'username' VARCHAR(50) NOT NULL UNIQUE,
-    'password' VARCHAR(255) NOT NULL,
-    created_at DATETIME DEFAULT CURRENT_TIMESTAMP
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` datetime DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
