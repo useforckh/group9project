7d596c1c784d0f1653bb3c49f3759c15cf0b882b
useforckh <117580613+useforckh@users.noreply.github.com>
2022-11-19 22:37:39 +0800

    Update myDB.sql

--- dump/myDB.sql
+++ dump/myDB.sql
@@ -16,15 +16,14 @@ INSERT INTO `Producttb` (`id`, `product_name`, `product_price`, `product_image`)
 
 
 CREATE TABLE `users` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
+  MODIFY `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT, AUTO_INCREMENT=1;
 
 
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
